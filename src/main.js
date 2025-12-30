e09623ca48f48197417cce43dfdcf76d3bd80497
arthurvicencio <artsvicencio@gmail.com>
2025-12-30 21:51:46 +0800

    refactor implementations

--- src/main.js
+++ src/main.js
@@ -17,7 +17,9 @@ const JTML = {
         // pipelines
         // decorator
 
-        const context = window[el.getAttribute("jt-scope")]?.();
+        const scopeInit = window[el.getAttribute("jt-scope")];
+        const context = scopeInit && scopeInit();
+
         const actors = el.querySelectorAll(`[${SupportedEvents.join("],[")}]`);
 
         for (const actor of actors) {
@@ -52,103 +54,93 @@ function bindEvents(scope, el, context) {
 }
 
 async function handleEvent(scope, el, eventVal, renderer, context, source, evt) {
-    evt?.preventDefault?.();
+    if (evt) {
+        evt.preventDefault();
+    }
 
-    const fn = window[eventVal];
-
-    const res = fn?.constructor?.name === "AsyncFunction" ? await fn(el, evt) : fn?.(el, evt);
+    const res = await fnRunner(eventVal, el, evt);
     if (res === false) {
         return;
     }
 
-    let data = context;
-    if (source.tagName === 'FORM' || source.tagName === 'A') {
+    if (!renderer) {
+        return;
+    }
+
+    if (["FORM", "A"].includes(source.tagName)) {
         const response = await jtRequester(scope, source);
         if (!response) {
             return;
         }
-        if (el.hasAttribute('jt-html')) {
-            data = response;
-        } else {
-            data = { ...data, ...response };
-        }
+        renderer(response);
+        return;
     }
 
-    renderer(data);
+    renderer(context);
 }
 
 function getRenderer(scope, requester) {
-    let compiledTemplate;
-
-    const template = !requester.hasAttribute('jt-html') ? resolveElFromAttr(scope, requester, "jt-render") || requester : null;
-    if (template) {
-        if (template._renderer) {
-            return template._renderer;
-        }
-
-        compiledTemplate = utils.compileTemplate(template);
+    const template = getTemplater(scope, requester);
+    if (!template) {
+        return;
     }
 
     const target = resolveElFromAttr(scope, requester, "jt-target") || requester;
     const swapper = getSwapper(requester);
 
-    const renderer = (data) => {
-        let dom = data;
+    return (data) => render(scope, template, data, swapper, target);
+}
 
-        if (compiledTemplate) {
-            dom = compiledTemplate(data);
-        }
-
-        if (!dom || !target) {
-            return;
-        }
-
-        swapper(target, dom);
-
-        JTML.bindDom(scope, target);
+function getTemplater(scope, requester) {
+    if (requester.hasAttribute('jt-html')) {
+        return data => data;
     }
 
-    if (template) {
-        template._renderer = renderer;
+    const template = resolveElFromAttr(scope, requester, "jt-render");
+    if (!template) {
+        return;
     }
 
-    return renderer;
+    if (template._compiled) {
+        return template._compiled;
+    }
+
+    const compiled = utils.compileTemplate(template);
+    template._compiled = compiled;
+
+    return compiled;
+}
+
+function render(scope, renderer, data, swapper, target) {
+    const dom = renderer(data);
+    if (!dom) {
+        return;
+    }
+
+    swapper(target, dom);
+
+    JTML.bindDom(scope, target);
 }
 
 function getSwapper(el) {
-    const DOMSwappers = {
-        replace: (target, dom) => target.replaceChildren(dom),
-        append: (target, dom) => target.appendChild(dom),
-        prepend: (target, dom) => target.prepend(dom),
-    };
-    const StringSwappers = {
-        replace: (target, domStr) => target.innerHTML = domStr,
-        append: (target, domStr) => target.innerHTML += domStr,
-        prepend: (target, domStr) => target.innerHTML = domStr + target.innerHTML,
-    };
     const swapType = el.getAttribute('jt-swap') || 'replace';
-    const swapperStrategy = el.hasAttribute('jt-html') ? StringSwappers : DOMSwappers;
-    return swapperStrategy[swapType];
+    return {
+        replace: (target, dom) => typeof dom === "string" ? target.innerHTML = dom : target.replaceChildren(dom),
+        append: (target, dom) => typeof dom === "string" ? target.innerHTML += dom : target.appendChild(dom),
+        prepend: (target, dom) => typeof dom === "string" ? target.innerHTML = dom + target.innerHTML : target.prepend(dom),
+    }[swapType];
 }
 
 async function httpRequest(requester) {
     const { url, options } = getFetchOptions(requester);
 
-    const preRequestHook = requester.getAttribute("jt-pre-request-fn");
-    if (preRequestHook) {
-        const preRequestFn = window[preRequestHook];
-        preRequestFn(requester, options);
-    }
-
     try {
+        await fnRunner(requester.getAttribute("jt-pre-request-fn"), requester, options);
+
         const res = await fetch(url, options);
         const body = await getResponseBody(res);
 
-        const postRequestHook = requester.getAttribute("jt-post-request-fn");
-        if (postRequestHook) {
-            const postRequestHookFn = window[postRequestHook];
-            postRequestHookFn(requester, res, body);
-        }
+        await fnRunner(requester.getAttribute("jt-post-request-fn"), requester, res, body);
 
         if (!res.ok) {
             throw {
@@ -159,15 +151,16 @@ async function httpRequest(requester) {
 
         return body;
     } catch (error) {
-        const requestErrorHook = requester.getAttribute("jt-request-error-fn");
-        if (requestErrorHook) {
-            const requestErrorHook = window[requestErrorHook];
-            requestErrorHook(requester, error);
-        }
+        await fnRunner(requester.getAttribute("jt-request-error-fn"), requester, error);
 
         console.error("[jtml] fetch failed:", url, error);
         throw error;
     }
+}
+
+async function fnRunner(name, ...args) {
+    const fn = window[name];
+    return (fn && fn(...args));
 }
 
 async function jtRequester(scope, requester) {
@@ -217,10 +210,6 @@ function getFetchOptions(requester) {
 };
 
 function extractRequestBody(el) {
-    if (el.tagName !== "FORM") {
-        return {};
-    }
-
     const formData = new FormData(el);
     const output = {};
 
@@ -231,7 +220,7 @@ function extractRequestBody(el) {
 };
 
 function getResponseBody(res) {
-    if (res.headers.get("Content-Type")?.includes("text/html")) {
+    if (res.headers.get("Content-Type").includes("text/html")) {
         return res.text();
     }
 
