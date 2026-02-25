5f092775e5ff465b96ce7b0939fe7931834c430f
arthurvicencio <artsvicencio@gmail.com>
2026-02-26 03:22:55 +0800

    remove esbuild fix rollup dev and update debugger

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { compileTemplate } from "./template-engine.js";
 
+const __DEBUG__ = process.env.NODE_ENV !== "production";
+
 const SupportedEvents = ["jt-click", "jt-submit", "jt-input", "jt-change", "jt-load"];
 
 const JTStore = {
@@ -43,50 +45,66 @@ const JTML = {
             actor._redered = true;
         }
 
-        if (__DEBUG__) {
-            debug(root);
-        }
+        debug(root);
     },
     store: JTStore,
 };
 
-if (__DEBUG__) {
-    const script = document.currentScript;
+const script = document.currentScript;
 
-    function debug(root) {
-        const url = new URL(script.src);
-        const params = url.searchParams;
+function debug(root) {
+    if (!__DEBUG__) {
+        return;
+    }
 
-        if (params.has("debug")) {
-            const actors = root.querySelectorAll(`[${SupportedEvents.join("],[")}]`);
+    const url = new URL(script.src);
+    const params = url.searchParams;
 
-            const jtProps = {
-                "jt-source": (actor) => actor.getAttribute("jt-source"),
-                "jt-store": (actor) => actor.hasAttribute("jt-store"),
-                "jt-html": (actor) => actor.hasAttribute("jt-html"),
-                "jt-render": (actor) => resolveElFromAttr(actor, "jt-render"),
-                "jt-target": (actor) => resolveElFromAttr(actor, "jt-target"),
-                "jt-swap": (actor) => actor.getAttribute("jt-swap") || "jt-replace",
-                "jt-after": (actor) => resolveElsFromAttr(actor, "jt-after"),
+    if (params.has("debug")) {
+        const actors = root.querySelectorAll(`[${SupportedEvents.join("],[")}]`);
+
+        const jtProps = {
+            "jt-source": (actor) => actor.getAttribute("jt-source"),
+            "jt-store": (actor) => actor.hasAttribute("jt-store"),
+            "jt-html": (actor) => actor.hasAttribute("jt-html"),
+            "jt-render": (actor) => resolveElFromAttr(actor, "jt-render"),
+            "jt-target": (actor) => resolveElFromAttr(actor, "jt-target"),
+            "jt-swap": (actor) => actor.getAttribute("jt-swap") || "jt-replace",
+            "jt-after": (actor) => resolveElsFromAttr(actor, "jt-after"),
+
+            "jt-loading": (actor) => resolveElFromAttr(actor, "jt-loading"),
+            "jt-error": (actor) => resolveElFromAttr(actor, "jt-error"),
+
+            "jt-pre-request-fn": (actor) => window[actor.getAttribute("jt-pre-request-fn")],
+            "jt-post-request-fn": (actor) => window[actor.getAttribute("jt-post-request-fn")],
+            "jt-request-error-fn": (actor) => window[actor.getAttribute("jt-request-error-fn")]
+        };
+
+        for (const event of SupportedEvents) {
+            jtProps[event] = (actor) => window[actor.getAttribute(event)];
+        }
+
+        for (const actor of actors) {
+
+            if (params.has("debug-only")) {
+                if (!actor.hasAttribute("jt-debug-only")) {
+                    continue;
+                }
             }
 
-            for (const actor of actors) {
-                const event = SupportedEvents.find(event => actor.hasAttribute(event));
-                const props = {
-                    actor,
-                    event,
-                };
+            const props = {
+                actor,
+            };
 
-                for (const jtProp in jtProps) {
-                    if (!actor.hasAttribute(jtProp) && !params.has("verbose")) {
-                        continue;
-                    }
-
-                    props[jtProp] = jtProps[jtProp](actor);
+            for (const jtProp in jtProps) {
+                if (!actor.hasAttribute(jtProp) && !params.has("debug-verbose")) {
+                    continue;
                 }
 
-                console.log("Processing JTML el:", props);
+                props[jtProp] = jtProps[jtProp](actor);
             }
+
+            console.log("Processing JTML el:", props);
         }
     }
 }
